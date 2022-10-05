918e45d0956916f0ff4ebd2910b9fd2809ca17ed
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2022-10-05 22:03:56 +0300

    Adding voice recognition and calculating mathematical expressions

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello!');
+const circleButton = document.querySelector('.circle-button');
+
+const list = document.querySelector('.list');
+
+circleButton.addEventListener('click', runSpeechRecognition);
+
+function runSpeechRecognition() {
+    const recognition = new webkitSpeechRecognition();
+
+    recognition.onstart = function () {
+        circleButton.innerHTML = '<p>Listening...</p>';
+        circleButton.classList.add('listening');
+        recognition.stop();
+    };
+
+    recognition.onspeechend = function () {
+        circleButton.innerHTML = '<p>Tap to speak</p>';
+        circleButton.classList.remove('listening');
+        recognition.stop();
+    };
+
+    recognition.onresult = function (event) {
+        console.log(event);
+        const transcript = event.results[0][0].transcript;
+
+        try {
+            let result = eval(transcript)
+            list.innerHTML = `<li>${transcript} = ${result}</li>` + list.innerHTML;
+        } catch (error) {
+            console.log(error);
+            alert(`the expression: '${transcript}' could not be evaluted. Perhabs is not the mathematical expression`);
+        };
+    };
+
+    recognition.start();
+};
